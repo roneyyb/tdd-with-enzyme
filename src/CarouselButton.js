fa175e1fa02e08c9b6b84d8e7398a56d04b6eeeb
Rajat Bothra <bothra.rajat08@gmail.com>
2023-07-10 13:48:59 +0530

    ✅ Added more test and passed them for button component

--- src/CarouselButton.js
+++ src/CarouselButton.js
@@ -2,10 +2,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const CarouselButton = ({ children }) => <button>{children}</button>;
+const CarouselButton = ({ children, onClick, className, dataAction }) => (
+    <button data-action={dataAction} onClick={onClick} className={className}>
+        {children}
+    </button>
+);
 
 CarouselButton.propTypes = {
     children: PropTypes.node.isRequired, // 1
+    onClick: PropTypes.func,
+    className: PropTypes.string,
+    dataAction: PropTypes.string,
 };
 
 export default CarouselButton;
--- src/tests/CarouselButton.test.js
+++ src/tests/CarouselButton.test.js
@@ -21,4 +21,14 @@ describe('CarouselButton', () => {
     it('passes children through the button', () => {
         expect(wrapper.prop('children')).toBe(text);
     });
+
+    it('passes other props through to the button', () => {
+        const onClick = () => {};
+        const className = 'my-carousel-button';
+        const dataAction = 'prev';
+        wrapper.setProps({ onClick, className, dataAction: dataAction }); // 1
+        expect(wrapper.prop('onClick')).toBe(onClick);
+        expect(wrapper.prop('className')).toBe(className);
+        expect(wrapper.prop('data-action')).toBe(dataAction);
+    });
 });
