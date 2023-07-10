f6ee5deb9e48efb7955bb44021aecb4e4a8544c3
Rajat Bothra <bothra.rajat08@gmail.com>
2023-07-10 15:19:21 +0530

    ✅ Fixed code to satisy test

--- src/CarouselSlide.js
+++ src/CarouselSlide.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-const CarouselSlide = () => (
+const CarouselSlide = ({ imgUrl, description, attribution }) => (
     <figure>
-        <img />
-        <figcaption />
+        <img src={imgUrl} />
+        <figcaption>
+            <strong>{description}</strong> {attribution}
+        </figcaption>
     </figure>
 );
+
+CarouselSlide.propTypes = {
+    imgUrl: PropTypes.string.isRequired,
+    description: PropTypes.node.isRequired,
+    attribution: PropTypes.node,
+};
 
 export default CarouselSlide;
