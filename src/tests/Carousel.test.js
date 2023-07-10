0e2f08ebef1aeb797685b44a0dc9510be3b90419
Rajat Bothra <bothra.rajat08@gmail.com>
2023-07-10 15:00:49 +0530

    🧪 Added failing test for CarouselSlide component

--- src/tests/Carousel.test.js
+++ src/tests/Carousel.test.js
@@ -2,6 +2,7 @@
 import React from 'react'; // 1
 import { shallow } from 'enzyme'; // 2
 import CarouselButton from '../CarouselButton';
+import CarouselSlide from '../CarouselSlide';
 
 describe('CarouselButton', () => {
     const text = 'Button text';
@@ -27,5 +28,20 @@ describe('CarouselButton', () => {
         expect(wrapper.prop('onClick')).toBe(onClick);
         expect(wrapper.prop('className')).toBe(className);
         expect(wrapper.prop('data-action')).toBe(dataAction);
+    });
+});
+
+describe('CarouselSlide', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallow(<CarouselSlide />);
+    });
+    it('renders a <figure>', () => {
+        expect(wrapper.type()).toBe('figure');
+    });
+
+    it('renders an <img> and a <figcaption> as children', () => {
+        expect(wrapper.childAt(0).type()).toBe('img');
+        expect(wrapper.childAt(1).type()).toBe('figcaption');
     });
 });
