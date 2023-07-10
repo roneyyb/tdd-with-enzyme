8454ac4f2b9e81b3b62f394f810070980fa2d159
Rajat Bothra <bothra.rajat08@gmail.com>
2023-07-10 13:17:58 +0530

    ➕ Added eslint & prettier

---
+++ src/tests/hello.test.js
@@ -0,0 +1,14 @@
+// src/tests/hello.test.js
+import React from 'react';
+
+describe('JSX', () => {
+    it('calls React.createElement', () => {
+        const createElementSpy = jest.spyOn(React, 'createElement'); // 1
+        <h1>Hello, JSX!</h1>;
+        expect(createElementSpy).toHaveBeenCalledWith(
+            'h1',
+            null,
+            'Hello, JSX!'
+        ); // 2
+    });
+});
