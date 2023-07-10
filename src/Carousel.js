17e008fd2c5aae95518f349ade85f58dedf24fc6
Rajat Bothra <bothra.rajat08@gmail.com>
2023-07-10 16:27:26 +0530

    ✅ Added test and updated code to satisfy test

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,12 +1,40 @@
 import React from 'react';
+import CarouselButton from './CarouselButton';
 
 class Carousel extends React.PureComponent {
     state = {
         currentIndex: 0,
     };
 
+    onClickPrevHandle = () => {
+        this.setState(({ currentIndex }) => ({
+            currentIndex: currentIndex != 0 ? currentIndex - 1 : 0,
+        }));
+    };
+
+    onClickNextHandle = () => {
+        this.setState(({ currentIndex }) => ({
+            currentIndex: currentIndex + 1,
+        }));
+    };
+
     render() {
-        return <div />;
+        return (
+            <div>
+                <CarouselButton
+                    onClick={this.onClickPrevHandle}
+                    dataAction={'prev'}
+                >
+                    Prev
+                </CarouselButton>
+                <CarouselButton
+                    onClick={this.onClickNextHandle}
+                    dataAction={'next'}
+                >
+                    Next
+                </CarouselButton>
+            </div>
+        );
     }
 }
 
--- src/tests/Carousel.test.js
+++ src/tests/Carousel.test.js
@@ -14,4 +14,36 @@ describe('Carousel', () => {
     it('check current index', () => {
         expect(wrapper.state('currentIndex')).toBe(0);
     });
+
+    it('render a CarouselButton labeled Prev', () => {
+        expect(
+            wrapper
+                .find('CarouselButton')
+                .at(0)
+                .prop('children')
+        ).toBe('Prev');
+    });
+    it('render a CarouselButton labeled Next', () => {
+        expect(
+            wrapper
+                .find('CarouselButton')
+                .at(1)
+                .prop('children')
+        ).toBe('Next');
+    });
+
+    it('decreases currentIndex when Prev button is clicked', () => {
+        wrapper.setState({ currentIndex: 1 });
+        wrapper.find('[dataAction="prev"]').simulate('click');
+        expect(wrapper.state('currentIndex')).toBe(0);
+        wrapper.find('[dataAction="prev"]').simulate('click');
+        expect(wrapper.state('currentIndex')).toBe(0);
+    });
+    it('increases currentIndex when button is clicked', () => {
+        wrapper.setState({ currentIndex: 1 });
+        wrapper.find('[dataAction="next"]').simulate('click');
+        expect(wrapper.state('currentIndex')).toBe(2);
+        wrapper.find('[dataAction="next"]').simulate('click');
+        expect(wrapper.state('currentIndex')).toBe(3);
+    });
 });
