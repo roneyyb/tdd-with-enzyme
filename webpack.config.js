038cc1b7fb3303e35de7c1c5740264102b2e9230
Rajat Bothra <bothra.rajat08@gmail.com>
2023-07-12 16:58:14 +0530

    ➕ Added webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+// webpack.config.js
+module.exports = {
+    //Using mode: 'development' provides good defaults for running code locally, optimizing for ease of debugging at the expense of bundle size and runtime performance. By contrast, mode: 'production' would give you a minified bundle.
+    mode: 'development', // 1
+    entry: {
+        //Each entry point defines a separate bundle for webpack to build. Here we’re telling webpack to build a bundle called carousel.js that contains src/Carousel.js and its dependencies.
+        carousel: './src/Carousel.js', // 2
+    },
+    // The module block is where we tell webpack how to treat different kinds of modules via an array of rules. A “module” in webpack can be any kind of file(JSON, CSS, even images)but we’re only concerned with JS modules, so we have just one rule.
+
+    module: {
+        // 3
+        // This rule says, “Use babel-loader for all .js files.” The loader will run each script through Babel with the config found in the root of our project.
+        rules: [
+            {
+                test: /\.js$/,
+                loader: require.resolve('babel-loader'), // 4
+            },
+        ],
+    },
+};
