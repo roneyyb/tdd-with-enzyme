63b0f5658fbbf80b540094aefeb3fa1007ea2a1d
roneyyb <bothra.rajat08@gmail.com>
2023-07-20 14:13:29 +0530

    Added: Styled component

--- src/CarouselSlide.js
+++ src/CarouselSlide.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Img = styled.img`
+  object-fit: contain;
+  width: 100%;
+
+  height: ${(props) =>
+    typeof props.imageHeight === "number"
+      ? `${props.imageHeight}px`
+      : props.imageHeight};
+`;
 
 const CarouselSlide = ({
-    imgUrl,
-    description,
-    attribution,
-    dataAction,
-    ...rest
+  imgUrl,
+  description,
+  attribution,
+  dataAction,
+  ...rest
 }) => (
-    <figure {...rest} data-action={dataAction}>
-        <img src={imgUrl} />
-        <figcaption>
-            <strong>{description}</strong> {attribution}
-        </figcaption>
-    </figure>
+  <figure {...rest} data-action={dataAction}>
+    <Img imageHeight={500} src={imgUrl} />
+    <figcaption>
+      <strong>{description}</strong> {attribution}
+    </figcaption>
+  </figure>
 );
 
 CarouselSlide.propTypes = {
-    imgUrl: PropTypes.string.isRequired,
-    description: PropTypes.node.isRequired,
-    attribution: PropTypes.node,
+  imgUrl: PropTypes.string.isRequired,
+  description: PropTypes.node.isRequired,
+  attribution: PropTypes.node,
 };
 
 export default CarouselSlide;
